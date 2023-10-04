947371a55df8eba03de52dc461c247b823e8446a
marcos-bene <64504672+marcos-bene@users.noreply.github.com>
2023-10-04 00:23:42 -0300

    Update PagamentoCartaoCredito.java

--- Strategy/src/br/edu/umfg/strategy/PagamentoCartaoCredito.java
+++ Strategy/src/br/edu/umfg/strategy/PagamentoCartaoCredito.java
@@ -37,18 +37,13 @@ public class PagamentoCartaoCredito implements PagamentoStrategy {
     }
 
     private boolean validarNumeroCartao() {
-        // Implemente a validação do número do cartão de crédito
-
-        // Remova espaços em branco e caracteres não numéricos
         String numeroCartaoLimpo = numeroCartao.replaceAll("[^0-9]", "");
 
-        // Verifique o comprimento mínimo do número do cartão
         if (numeroCartaoLimpo.length() != 16) {
             System.out.println("Número de cartão de crédito inválido. O número deve conter 16 dígitos.");
             return false;
         }
 
-        // Implemente a validação do algoritmo de Luhn (soma de verificação)
         int sum = 0;
         boolean doubleDigit = false;
 
