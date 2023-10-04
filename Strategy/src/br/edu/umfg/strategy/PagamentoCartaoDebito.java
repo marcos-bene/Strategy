0a0ae724dd9c77be3e47ab116cd717acf8abf726
marcos-bene <64504672+marcos-bene@users.noreply.github.com>
2023-10-04 00:24:29 -0300

    PagamentoCartaoDebito.java

--- Strategy/src/br/edu/umfg/strategy/PagamentoCartaoDebito.java
+++ Strategy/src/br/edu/umfg/strategy/PagamentoCartaoDebito.java
@@ -26,18 +26,13 @@ public class PagamentoCartaoDebito implements PagamentoStrategy {
     }
 
     private boolean validarNumeroCartao() {
-        // Implemente a validação do número do cartão de débito
-
-        // Remova espaços em branco e caracteres não numéricos
         String numeroCartaoLimpo = numeroCartao.replaceAll("[^0-9]", "");
 
-        // Verifique o comprimento mínimo do número do cartão
         if (numeroCartaoLimpo.length() != 16) {
             System.out.println("Número de cartão de débito inválido. O número deve conter 16 dígitos.");
             return false;
         }
 
-        // Implemente a validação do algoritmo de Luhn (soma de verificação)
         int sum = 0;
         boolean doubleDigit = false;
 
@@ -70,8 +65,7 @@ public class PagamentoCartaoDebito implements PagamentoStrategy {
     }
 
     private boolean isValidCPF(String cpf) {
-        cpf = cpf.replaceAll("[^0-9]", ""); // Remove caracteres não numéricos
-
+        cpf = cpf.replaceAll("[^0-9]", ""); 
         if (cpf.length() != 11) {
             return false;
         }
